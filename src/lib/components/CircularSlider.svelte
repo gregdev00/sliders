31871cc911d9e2b71118858645a45e60348b66fd
Gergő Szabó <batcavechannel@gmail.com>
2026-06-18 21:06:15 +0200

    circular slider layout prepare

---
+++ src/lib/components/CircularSlider.svelte
@@ -0,0 +1,3 @@
+<div class="flex flex-col items-center gap-2 bg-bg-elev border border-border rounded-main p-5 mb-4">
+	CircularSlider
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import CircularSlider from '$lib/components/CircularSlider.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import Modal from '$lib/components/Modal.svelte';
 	import TaskList from '$lib/components/TaskList.svelte';
@@ -8,8 +9,16 @@
 
 <Header onHelpClick={() => (helpModalOpen = true)} />
 
-<div class="mb-3.5">
-	<TaskList />
+<div class="grid grid-cols-[380px_1fr] gap-6 items-start px-5 py-4">
+	<div class="sticky top-32.5 self-start">
+		<CircularSlider />
+	</div>
+	<div>
+		<div class="flex gap-2 mb-3.5">Settings</div>
+		<div class="mb-3.5">
+			<TaskList />
+		</div>
+	</div>
 </div>
 
 <Modal bind:isOpen={helpModalOpen}>asd</Modal>
