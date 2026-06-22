46154c4bc1503bc4938e78adc7d86dfa5bac6dde
Gergő Szabó <batcavechannel@gmail.com>
2026-06-22 15:07:56 +0200

    animacio javitasa

--- src/lib/components/ToastList.svelte
+++ src/lib/components/ToastList.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { toastService } from '$lib/services/ToastService.svelte';
 	import { fly } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
 	import Toast from './Toast.svelte';
 
-	// Reverse so the newest toast is on top
-	const activeToasts = $derived([...toastService.toasts].reverse());
+	// get 4 most recent toasts
+	const activeToasts = $derived([...toastService.toasts].reverse().slice(0, 4));
 </script>
 
 <div
@@ -12,20 +13,22 @@
            z-200 grid place-items-center w-max pointer-events-none"
 >
 	{#each activeToasts as toast, i (toast.id)}
-		{#if i < 4}
+		<div
+			animate:flip={{ duration: 250 }}
+			class="col-start-1 row-start-1 transition-all duration-300 ease-out pointer-events-auto"
+			style="
+                --index: {i};
+                transform: translateY(calc(var(--index) * -8px)) scale(calc(1 - var(--index) * 0.04));
+                z-index: {100 - i};
+                opacity: {i === 3 ? 0 : 1 - i * 0.15};
+            "
+		>
 			<div
-				in:fly={{ y: 30, duration: 280, opacity: 0 }}
+				in:fly={{ y: 40, duration: 300, opacity: 0 }}
 				out:fly={{ y: -15, duration: 200, opacity: 0 }}
-				class="col-start-1 row-start-1 transition-all duration-300 ease-out pointer-events-auto"
-				style="
-                    --index: {i};
-                    transform: translateY(calc(var(--index) * -8px)) scale(calc(1 - var(--index) * 0.04));
-                    z-index: {100 - i};
-                    opacity: {i === 3 ? 0 : 1 - i * 0.15};
-                "
 			>
 				<Toast {...toast} />
 			</div>
-		{/if}
+		</div>
 	{/each}
 </div>
